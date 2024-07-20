0a29eda427a59eaac58a9b1cbbb0437b717f069e
poojajahagirdar22 <pooja.jahag@gmail.com>
2024-07-20 23:17:31 +0400

    Update main Java file with new features

--- src/com/example/eventmanagement/EventManagementSystemApplication.java
+++ src/com/example/eventmanagement/EventManagementSystemApplication.java
@@ -1,13 +1,17 @@
 package com.example.eventmanagement;
 
+import com.example.eventmanagement.controllers.AttendeeController;
 import com.example.eventmanagement.controllers.UserController;
+import com.example.eventmanagement.controllers.EventController;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class EventManagementSystemApplication {
     public static void main(String[] args) {
         UserController userController = new UserController();
-
+        EventController eventController = new EventController();
+        AttendeeController attendeeController = new AttendeeController();
         // Test user registration
         //1: pass
         userController.registerUser("abc", "ab.cd1@gmail.com", "Password@1234");
@@ -35,5 +39,36 @@ public class EventManagementSystemApplication {
         userController.UpdateUserProfile("1", "abcd", "ab.cd@gmail.com");
 
 
-        }
+        //Test event creation
+        //1:
+        eventController.createEvent("Conference", "Tech Conference", LocalDateTime.now().plusDays(10), "Pune", "1");
+
+        //Test list of event
+        //1:
+        eventController.listAllEvents();
+
+        //Test view event details
+        //1:
+        eventController.viewEventDetails("1");
+
+        //Test update event
+        //1:
+        eventController.editEvent("1", "New Conference", "New data for Tech Conference", LocalDateTime.now().plusDays(15), "Gulbarga");
+
+
+        //Test deletion of event
+        //1:
+        eventController.deleteEvent("1");
+
+        // Test registering attendees
+        attendeeController.RegisterAttendee("1", "1");
+        attendeeController.RegisterAttendee("2", "1");
+
+        // Test viewing attendee list
+        attendeeController.ViewAttendeeList("1");
+
+        // Test sending reminders
+        attendeeController.SendReminder("1");
+
     }
+}
